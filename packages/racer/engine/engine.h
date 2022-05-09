028eb8cacc198c5a892bf261166a9ecf0a6e27c3
Harrison Marshall <harry130899@hotmail.com>
2022-05-09 15:14:03 +1000

    Engine: Handled window resizes

--- packages/racer/engine/engine.h
+++ packages/racer/engine/engine.h
@@ -7,6 +7,11 @@
 namespace engine
 {
 
+namespace detail
+{
+  void scale_view_to_window_size(sf::RenderWindow&);
+}
+
 float const COORDINATE_SPACE_WIDTH = 200;
 float const COORDINATE_SPACE_HEIGHT = 200;
 
@@ -14,7 +19,11 @@ App create_app(std::string_view name) { return App{ .name = name }; }
 
 void run_app(App const& app)
 {
-  sf::Window window(sf::VideoMode(1920, 1080), app.name.cbegin());
+  sf::RenderWindow window(sf::VideoMode(1920, 1080), app.name.cbegin());
+  sf::View view(sf::FloatRect(0, 0, COORDINATE_SPACE_WIDTH, COORDINATE_SPACE_HEIGHT));
+  window.setView(view);
+
+  detail::scale_view_to_window_size(window);
 
   while (window.isOpen()) {
     window.clear();
@@ -24,6 +33,7 @@ void run_app(App const& app)
     while (window.pollEvent(event)) {
 
       if (event.type == sf::Event::Closed) window.close();
+      if (event.type == sf::Event::Resized) { detail::scale_view_to_window_size(window); }
     }
 
     for (auto entity : app.entities) { entity->render(window); }
@@ -52,5 +62,32 @@ std::shared_ptr<EntityType> spawn_entity(App& app, EntityArgs... args)
 
   return entity;
 }
+
+namespace detail
+{
+  void scale_view_to_window_size(sf::RenderWindow& window)
+  {
+
+    sf::View theView = window.getView();
+
+    auto const [width, height] = window.getSize();
+
+
+    if (width < height) {
+      const float windowRatio = static_cast<float>(height) / width;
+      const float newViewHeight = theView.getSize().x * windowRatio;
+      theView.setSize({ theView.getSize().x, newViewHeight });
+
+      window.setView(theView);
+      return;
+    }
+
+    const float windowRatio = static_cast<float>(width) / height;
+    const float newViewWidth = theView.getSize().y * windowRatio;
+    theView.setSize({ newViewWidth, theView.getSize().y });
+
+    window.setView(theView);
+  }
+}// namespace detail
 
 }// namespace engine
