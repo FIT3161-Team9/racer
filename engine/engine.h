93d9007db4b932633e50c72a71840739fa98fc85
Harrison Marshall <harry130899@hotmail.com>
2022-05-15 23:10:21 +1000

    Engine: Greatly improved the main loop
    Previously, we would try and render every iteration of the main loop.
    This is likely to lead to very inconsistent behaviour between different
    systems.
    
    The idea here, is to separate update logic from render logic, and only
    render at most 60 times per seconds. We are basically capping the frame
    time at 12ms, but still updating the game at the same rate internally.
    
    This will allow us to not waste time rendering the scene excessively,
    and focus on running the physics simulation

--- engine/engine.h
+++ engine/engine.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <cstdint>
 
 #include "app.h"
 
@@ -13,6 +14,7 @@ namespace detail
   void scale_view_to_window_size(sf::RenderWindow&);
 }// namespace detail
 
+std::uint64_t MS_PER_FRAME = 12;
 float const COORDINATE_SPACE_WIDTH = 2000;
 float const COORDINATE_SPACE_HEIGHT = 1000;
 auto const ASPECT_RATIO = COORDINATE_SPACE_WIDTH / COORDINATE_SPACE_HEIGHT;
@@ -27,11 +29,24 @@ void run_app(App const& app)
 
   detail::scale_view_to_window_size(window);
 
+  sf::Clock clock;
+  std::uint64_t total_time_since_render{};
   while (window.isOpen()) {
-    window.clear();
     detail::handle_events(window);
-    for (auto entity : app.entities) { entity->render(window); }
-    window.display();
+    sf::Time const elapsed = clock.restart();
+    std::uint64_t const elapsed_milliseconds = elapsed.asMilliseconds();
+
+    total_time_since_render += elapsed_milliseconds;
+
+    // We would update the entities here:
+    // for (auto entity : app.entities) { entity->update(elapsed_milliseconds); }
+
+    if (total_time_since_render > MS_PER_FRAME) {
+      total_time_since_render -= MS_PER_FRAME;
+      window.clear();
+      for (auto entity : app.entities) { entity->render(window); }
+      window.display();
+    }
   }
 }
 
