0d84865597900d7b0c2a5af35f50c18104388c50
Harrison Marshall <harry130899@hotmail.com>
2022-07-27 11:24:12 +1000

    Implemented a way of storing fonts in render context
    Also added primitives for rendering text.
    
    Although we don't actually render any text yet

--- engine/render_context.h
+++ engine/render_context.h
@@ -5,31 +5,37 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <entt/entt.hpp>
 #include <iostream>
 #include <map>
 #include <string>
 
+#include "app.h"
+#include "text.h"
 #include "texture.h"
 
 // Forward declare for friend
 namespace engine
 {
-template<typename AppState, typename StartupFn>
+template<typename StartupFn>
 void run_app(App const&, StartupFn);
 }
 
 class RenderContext
 {
   using FilePath = std::string;
+  using FontCache = std::map<FilePath, sf::Font>;
   using TextureCache = std::map<FilePath, sf::Texture>;
   using ShaderCache = std::map<FilePath, sf::Shader>;
   TextureCache m_texture_cache{};
+  FontCache m_font_cache{};
   ShaderCache m_shader_cache{};
+  entt::registry& m_entity_registry;
 
-  template<typename AppState, typename StartupFn>
+  template<typename StartupFn>
   friend void engine::run_app(App const&, StartupFn);
 
-  RenderContext() {}
+  RenderContext(entt::registry& entity_registry) : m_entity_registry(entity_registry) {}
 
 public:
   sf::Texture& get_or_load_texture(Texture const& texture)
@@ -42,5 +48,23 @@ public:
       m_texture_cache.insert({ texture.path, sf_texture });
     }
     return m_texture_cache.at(texture.path);
+  }
+
+  sf::Font& get_or_load_font(Text const& text)
+  {
+    if (!m_font_cache.contains(text.font)) {
+      sf::Font sf_font{};
+      if (!sf_font.loadFromFile(text.font)) {
+        // TODO: Handle font loading errors
+      }
+      m_font_cache.insert({ text.font, sf_font });
+    }
+    return m_font_cache.at(text.font);
+  }
+
+  template<typename ComponentType>
+  ComponentType* get_component(entt::entity entity)
+  {
+    return m_entity_registry.try_get<ComponentType>(entity);
   }
 };
