d78bb2aec69a438610bd23badbdaa29480f493e8
Harrison Marshall <harry130899@hotmail.com>
2022-07-27 11:29:27 +1000

    Introduced the concept of queries
    Queries are now how we drive our systems - you specify that a system
    depends on a query, and then when the system runs, the query returns any
    entities + components that match the query

--- engine/app_commands.h
+++ engine/app_commands.h
@@ -6,51 +6,35 @@
 #include "app.h"
 #include "entity.h"
 #include "event.h"
+#include "query.h"
 
-template<typename StateType>
 class AppCommands;
 
 namespace engine
 {
-template<typename AppState, typename StartupFn>
+template<typename StartupFn>
 void run_app(App const&, StartupFn);
 namespace detail
 {
-  template<typename AppState>
-  void handle_events(sf::RenderWindow& window, AppCommands<AppState>& app_commands);
+  void handle_events(sf::RenderWindow& window, AppCommands& app_commands);
 }
 };// namespace engine
 
-template<typename AppStateType>
 class AppCommands
 {
   std::vector<std::function<void()>> m_systems{};
   std::vector<std::function<void(Event)>> m_event_systems{};
-  std::vector<std::function<void(AppStateType, AppStateType)>> m_state_change_listeners{};
   entt::registry& m_registry;
-  AppStateType m_state;
-  entt::dispatcher m_dispatcher_state_change_event{};
+  entt::registry m_resource_registry{};
+  entt::entity m_resource_entity;
 
-  struct StateChangeEvent
-  {
-    AppStateType previous_state;
-    AppStateType new_state;
-  };
-
-  template<typename AppState, typename StartupFn>
+  template<typename StartupFn>
   friend void engine::run_app(App const&, StartupFn);
-  template<typename AppState>
-  friend void engine::detail::handle_events(sf::RenderWindow&, AppCommands<AppState>&);
+  friend void engine::detail::handle_events(sf::RenderWindow&, AppCommands&);
 
-  void notify_listeners_of_state_change(StateChangeEvent& change)
+  explicit AppCommands(entt::registry& registry) : m_registry(registry)
   {
-    for (auto listener : m_state_change_listeners) { listener(change.previous_state, change.new_state); }
-  }
-
-  AppCommands(entt::registry& registry) : m_registry(registry)
-  {
-    m_dispatcher_state_change_event.sink<StateChangeEvent>()
-      .template connect<&AppCommands<AppStateType>::notify_listeners_of_state_change>(*this);
+    m_resource_entity = m_resource_registry.create();
   }
 
   void run_systems()
@@ -60,6 +44,7 @@ class AppCommands
 
 public:
   Entity spawn() { return { m_registry.create(), m_registry }; }
+  void destroy(entt::entity entity) { m_registry.destroy(entity); }
 
   template<Event::EventType event_type, typename... ComponentTypes, typename Fn>
   void add_system(Fn function)
@@ -78,26 +63,60 @@ public:
   void add_system(Fn function)
   {
     m_systems.push_back([=]() {
-      auto view = m_registry.view<ComponentTypes...>();
-      function(view);
+      if constexpr (!sizeof...(ComponentTypes)) {
+        function();
+      } else {
+        auto view = m_registry.view<ComponentTypes...>();
+        function(view);
+      }
     });
   }
 
-  template<typename Fn>
-  void on_state_change(Fn listener)
+  template<typename... ResourceQueryTypes, typename... EntityQueryTypes, typename Fn>
+  void add_system(ResourceQuery<ResourceQueryTypes...>, Query<EntityQueryTypes...>, Fn fn)
   {
-    m_state_change_listeners.push_back(listener);
+    auto resource_tuple = *m_resource_registry.view<ResourceQueryTypes...>().each().begin();
+    auto entity_view = m_registry.view<EntityQueryTypes...>();
+    m_systems.push_back([=]() { fn(resource_tuple, entity_view); });
+    return;
   }
 
-  void set_state(AppStateType value)
+  template<typename... ResourceQueryTypes, typename... QueryOneTypes, typename... QueryTwoTypes, typename Fn>
+  void add_system(ResourceQuery<ResourceQueryTypes...>, Query<QueryOneTypes...>, Query<QueryTwoTypes...>, Fn fn)
   {
-    // Shortcircuit if the value hasn't changed
-    if (value == m_state) return;
-    m_dispatcher_state_change_event.trigger(StateChangeEvent{ .previous_state = m_state, .new_state = value });
-    m_state = value;
+    auto resource_tuple = *m_resource_registry.view<ResourceQueryTypes...>().each().begin();
+    auto view_one = m_registry.view<QueryOneTypes...>();
+    auto view_two = m_registry.view<QueryTwoTypes...>();
+    m_systems.push_back([=]() { fn(resource_tuple, view_one, view_two); });
+    return;
   }
 
-  AppStateType current_state() { return m_state; }
+  template<typename... QueryOneTypes, typename... QueryTwoTypes, typename Fn>
+  void add_system(Query<QueryOneTypes...>, Query<QueryTwoTypes...>, Fn fn)
+  {
+    auto view_one = m_registry.view<QueryOneTypes...>();
+    auto view_two = m_registry.view<QueryTwoTypes...>();
+    m_systems.push_back([=]() { fn(view_one, view_two); });
+    return;
+  }
+
+
+  template<typename... QueryTypes, typename Fn>
+  void add_system(Query<QueryTypes...>, Fn fn)
+  {
+    auto view = m_registry.view<QueryTypes...>();
+    m_systems.push_back([=]() { fn(view); });
+    return;
+  }
+
+  template<Event::EventType event_type, typename... QueryTypes, typename Fn>
+  void add_system(Query<QueryTypes...>, Fn function)
+  {
+    auto view = m_registry.view<QueryTypes...>();
+    m_event_systems.push_back([=](Event event) {
+      if (event.type == event_type) function(event, view);
+    });
+  }
 
   template<typename Plugin>
   void add_plugin(Plugin plugin)
@@ -109,5 +128,19 @@ public:
   Component* component(entt::entity entity)
   {
     return m_registry.try_get<Component>(entity);
+  }
+
+  template<typename Resource, typename... ResourceArgs>
+  void add_resource(ResourceArgs... args)
+  {
+    if (!m_resource_registry.try_get<Resource>(m_resource_entity)) {
+      m_resource_registry.emplace<Resource>(m_resource_entity, std::forward<ResourceArgs>(args)...);
+    }
+  }
+
+  template<typename Resource>
+  Resource* get_resource()
+  {
+    return m_resource_registry.try_get<Resource>(m_resource_entity);
   }
 };
