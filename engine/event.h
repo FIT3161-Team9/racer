3e37e8339b76f12f04e5bb9984ed36b2416e202d
Harrison Marshall <harry130899@hotmail.com>
2022-07-27 11:24:59 +1000

    Added key pressed and key released events

--- engine/event.h
+++ engine/event.h
@@ -5,7 +5,7 @@
 struct Event
 {
   // Type Definitions
-  enum class EventType { MouseMoved, MouseButtonPressed, MouseButtonReleased };
+  enum class EventType { MouseMoved, MouseButtonPressed, MouseButtonReleased, KeyPressed, KeyReleased };
   struct MouseMoved
   {
     sf::Vector2f location;
@@ -21,12 +21,24 @@ struct Event
     sf::Vector2f location;
   };
 
+  struct KeyPressed
+  {
+    sf::Keyboard::Key key;
+  };
+
+  struct KeyReleased
+  {
+    sf::Keyboard::Key key;
+  };
+
   // Members
   EventType type;
   union {
     MouseMoved mouse_moved;
     MouseButtonPressed mouse_button_pressed;
     MouseButtonReleased mouse_button_released;
+    KeyPressed key_pressed;
+    KeyReleased key_released;
   };
 };
 
@@ -34,19 +46,29 @@ struct Event
 namespace event
 {
 
-Event mouse_moved(sf::Vector2f location)
+inline Event mouse_moved(sf::Vector2f location)
 {
   return Event{ .type = Event::EventType::MouseMoved, .mouse_moved = { .location = location } };
 }
 
-Event mouse_button_pressed(sf::Vector2f location)
+inline Event mouse_button_pressed(sf::Vector2f location)
 {
   return Event{ .type = Event::EventType::MouseButtonPressed, .mouse_button_pressed = { .location = location } };
 }
 
-Event mouse_button_released(sf::Vector2f location)
+inline Event mouse_button_released(sf::Vector2f location)
 {
   return Event{ .type = Event::EventType::MouseButtonReleased, .mouse_button_released = { .location = location } };
+}
+
+inline Event key_pressed(sf::Keyboard::Key key)
+{
+  return Event{ .type = Event::EventType::KeyPressed, .key_pressed = { .key = key } };
+}
+
+inline Event key_released(sf::Keyboard::Key key)
+{
+  return Event{ .type = Event::EventType::KeyReleased, .key_released = { .key = key } };
 }
 
 }// namespace event
