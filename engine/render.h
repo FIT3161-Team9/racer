86adedc2667830d4b8de63c7d60f8c0bfef5d740
Harrison Marshall <harry130899@hotmail.com>
2022-08-09 09:45:45 +1000

    Engine: Added letter spacing parameter to Text component

--- engine/render.h
+++ engine/render.h
@@ -129,6 +129,7 @@ inline void text(RenderContext& render_context,
   sf_text.setFont(render_context.get_or_load_font(text));
   sf_text.setPosition(window::to_screen_space(transform.value));
   sf_text.setCharacterSize(text.character_size);
+  sf_text.setLetterSpacing(text.letter_spacing);
   sf_text.setString(text.content.data());
   sf_text.setFillColor(render_utils::convert_colour(colour));
 
