f5d94983cb8d25bb8ab8235cff489b607bb75f68
Harrison Marshall <harry130899@hotmail.com>
2022-08-09 22:36:01 +1000

    Game: Simplfied splash screen logic and added documentation

--- game/src/splash_screen.h
+++ game/src/splash_screen.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <entt/entt.hpp>
 #include <filesystem>
 #include <iostream>
 #include <vector>
@@ -17,60 +18,73 @@
 #include <engine/window.h>
 
 #include "./game_state.h"
-
+#include "./utils.h"
 
 namespace splash_screen
 {
 
+void spawn_ui(AppCommands&);
+void destroy_ui(AppCommands&, entt::entity flex_container, Children&);
+
+/// This plugin implements the game's splash screen
 inline void plugin(AppCommands& app_commands)
 {
-  // Listen for enter key
+  // Spawn the background
+  {
+    using utils::u8;
+    app_commands.spawn()
+      .add_component<Rectangle>(sf::Vector2f{ window::COORDINATE_SPACE_WIDTH, window::COORDINATE_SPACE_HEIGHT })
+      .add_component<Colour>(u8(255), u8(237), u8(237))
+      .add_component<Transform>(sf::Vector2f{ 0.f, 0.f });
+  }
+
+  // Listen for the "enter" key
   app_commands.template add_system<Event::EventType::KeyReleased>(
     ResourceQuery<GameState>{}, Query<layout::Flex>{}, [&](auto& event, auto& resource_tuple, auto& flex_query) {
       auto&& [_, game_state] = resource_tuple;
-      if (event.key_released.key == sf::Keyboard::Key::Enter
-          && game_state.current_screen == GameState::CurrentScreen::SplashScreen) {
-        std::cout << "Would transition to start screen\n";
-        // update the game state
-        // destroy the splash screen UI
-        // create the main menu UI
-        for (auto&& [flex_entity, _] : flex_query.each()) {
-          auto* children = app_commands.component<Children>(flex_entity);
-          if (children) {
-            for (auto child : children->children) { app_commands.destroy(child); }
-          }
-          app_commands.destroy(flex_entity);
-        }
+
+      // If the key that was pressed wasn't "enter", or the current screen isn't
+      // the splash screen, do nothing
+      if (event.key_released.key != sf::Keyboard::Key::Enter
+          || game_state.current_screen != GameState::CurrentScreen::SplashScreen) {
+        return;
       }
+
+      game_state.current_screen = GameState::CurrentScreen::MainMenu;
+      auto flex_container = *flex_query.begin();
+      destroy_ui(app_commands, flex_container, *app_commands.component<Children>(flex_container));
+
+      // TODO: Spawn UI for main menu
     });
 
-  // Spawn UI
-  app_commands.spawn()
-    .template add_component<Rectangle>(sf::Vector2f{ window::COORDINATE_SPACE_WIDTH, window::COORDINATE_SPACE_HEIGHT })
-    .template add_component<Colour>(static_cast<uint8_t>(255), static_cast<uint8_t>(237), static_cast<uint8_t>(237))
-    .template add_component<Transform>(sf::Vector2f{ 0.f, 0.f });
+  spawn_ui(app_commands);
+}
 
+/// Destroy the UI for the splash screen
+inline void destroy_ui(AppCommands& app_commands, entt::entity flex_container, Children& children)
+{
+  for (auto child : children.children) { app_commands.destroy(child); }
+  app_commands.destroy(flex_container);
+}
+
+/// Create the UI for ths splash screen
+inline void spawn_ui(AppCommands& app_commands)
+{
+  using utils::u32;
   auto title = app_commands.spawn()
-                 .template add_component<Text>(
-                   std::filesystem::path("./assets/Inter-Black.ttf").string(), "RACER", static_cast<uint32_t>(97), 2.5f)
-                 .template add_component<Colour>(colour::black())
-                 .template add_component<layout::Margin>(layout::margin_top(350.f));
+                 .add_component<Text>(utils::INTER_BLACK, "RACER", u32(97), 2.5f)
+                 .add_component<Colour>(colour::black())
+                 .add_component<layout::Margin>(layout::margin_top(350.f));
 
   auto subtitle = app_commands.spawn()
-                    .template add_component<Text>(std::filesystem::path("./assets/Inter-SemiBold.ttf").string(),
-                                                  "SIMULATION-BASED RACING",
-                                                  static_cast<uint32_t>(21),
-                                                  0.85f)
-                    .template add_component<Colour>(colour::black())
-                    .template add_component<layout::Margin>(layout::margin_top(45.f));
+                    .add_component<Text>(utils::INTER_SEMI_BOLD, "SIMULATION-BASED RACING", u32(21), 0.85f)
+                    .add_component<Colour>(colour::black())
+                    .add_component<layout::Margin>(layout::margin_top(45.f));
 
   auto prompt = app_commands.spawn()
-                  .template add_component<Text>(std::filesystem::path("./assets/Inter-SemiBold.ttf").string(),
-                                                "PRESS ENTER BUTTON TO START",
-                                                static_cast<uint32_t>(28),
-                                                2.5f)
-                  .template add_component<Colour>(colour::black())
-                  .template add_component<layout::Margin>(layout::margin_top(100.f));
+                  .add_component<Text>(utils::INTER_SEMI_BOLD, "PRESS ENTER BUTTON TO START", u32(28), 2.5f)
+                  .add_component<Colour>(colour::black())
+                  .add_component<layout::Margin>(layout::margin_top(100.f));
 
   app_commands.spawn()
     .template add_component<layout::Flex>(layout::Flex::Direction::Vertical, layout::Flex::Alignment::Center)
