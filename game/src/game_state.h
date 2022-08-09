d9aa952c5105763b9cef7db89a00eefd3d1d6cfc
Harrison Marshall <harry130899@hotmail.com>
2022-08-09 22:36:01 +1000

    Game: Simplfied splash screen logic and added documentation

--- game/src/game_state.h
+++ game/src/game_state.h
@@ -1,7 +1,9 @@
 #pragma once
 
+/// The global state of the game
 struct GameState
 {
+  /// The current screen to be rendered
   enum class CurrentScreen {
     SplashScreen,
     MainMenu,
---
+++ game/src/utils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+#include <filesystem>
+
+namespace utils
+{
+
+consteval uint8_t u8(auto n) { return static_cast<uint8_t>(n); }
+consteval uint32_t u32(auto n) { return static_cast<uint32_t>(n); }
+
+const std::string INTER_BLACK = std::filesystem::path("./assets/Inter-Black.ttf").string();
+const std::string INTER_SEMI_BOLD = std::filesystem::path("./assets/Inter-SemiBold.ttf").string();
+
+};// namespace utils
