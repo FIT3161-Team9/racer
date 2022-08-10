d9d18568c0f4a122225526c5d26c9723cf06be99
Kurii <marcus.kuri@y7mail.com>
2022-08-10 11:59:32 +1000

    Game: Added the velocity component and velocity tests

--- game/src/utils.h
+++ game/src/utils.h
@@ -6,8 +6,8 @@
 namespace utils
 {
 
-consteval uint8_t u8(auto n) { return static_cast<uint8_t>(n); }
-consteval uint32_t u32(auto n) { return static_cast<uint32_t>(n); }
+uint8_t u8(auto n) { return static_cast<uint8_t>(n); }
+uint32_t u32(auto n) { return static_cast<uint32_t>(n); }
 
 const std::string INTER_BLACK = std::filesystem::path("./assets/Inter-Black.ttf").string();
 const std::string INTER_SEMI_BOLD = std::filesystem::path("./assets/Inter-SemiBold.ttf").string();
---
+++ game/tests/velocity.cpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <gtest/gtest.h>
+
+
+#include "../src/velocity.h"
+
+
+TEST(ApplyVelocity, ZeroVelocity)
+{
+  auto const result =
+    velocity::apply_velocity(Velocity{ sf::Vector2f{ 0.f, 0.f } }, Transform{ sf::Vector2f{ 0.f, 0.f } }, 100);
+  EXPECT_FLOAT_EQ(result.value.x, 0.f);
+  EXPECT_FLOAT_EQ(result.value.y, 0.f);
+}
+
+TEST(ApplyVelocity, ZeroTimePassed)
+{
+  auto const result =
+    velocity::apply_velocity(Velocity{ sf::Vector2f{ 10.f, 10.f } }, Transform{ sf::Vector2f{ 0.f, 0.f } }, 0);
+  EXPECT_FLOAT_EQ(result.value.x, 0.f);
+  EXPECT_FLOAT_EQ(result.value.y, 0.f);
+}
+
+TEST(ApplyVelocity, OneSecondRight)
+{
+  auto const result =
+    velocity::apply_velocity(Velocity{ sf::Vector2f{ 10.f, 0.f } }, Transform{ sf::Vector2f{ 0.f, 0.f } }, 1000);
+  EXPECT_FLOAT_EQ(result.value.x, 10.f);
+  EXPECT_FLOAT_EQ(result.value.y, 0.f);
+
+  auto const result2 =
+    velocity::apply_velocity(Velocity{ sf::Vector2f{ -10.f, 0.f } }, Transform{ sf::Vector2f{ 0.f, 0.f } }, 1000);
+  EXPECT_FLOAT_EQ(result2.value.x, -10.f);
+  EXPECT_FLOAT_EQ(result2.value.y, 0.f);
+
+  auto const result3 =
+    velocity::apply_velocity(Velocity{ sf::Vector2f{ 0.f, 10.f } }, Transform{ sf::Vector2f{ 0.f, 0.f } }, 1000);
+  EXPECT_FLOAT_EQ(result3.value.x, 0.f);
+  EXPECT_FLOAT_EQ(result3.value.y, 10.f);
+
+  auto const result4 =
+    velocity::apply_velocity(Velocity{ sf::Vector2f{ 0.f, -10.f } }, Transform{ sf::Vector2f{ 0.f, 0.f } }, 1000);
+  EXPECT_FLOAT_EQ(result4.value.x, 0.f);
+  EXPECT_FLOAT_EQ(result4.value.y, -10.f);
+}
