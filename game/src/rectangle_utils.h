1df4b715ff0b72cf8fc60b724fe29f74b0ab32d1
Harrison Marshall <harry130899@hotmail.com>
2022-08-17 12:10:17 +1000

    Game: Added some rectangle utils and some tests for them
    We now have a way to get the corners of a rectangle as well as the
    segmetns that make up the outline of a rectangle

---
+++ game/src/rectangle_utils.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+#include <engine/rectangle.h>
+#include <engine/rotation.h>
+#include <engine/transform.h>
+
+#include "segment.h"
+
+namespace rectangle_utils
+{
+
+/// The four corners of a rectangle
+inline std::vector<sf::Vector2f>
+  corners(Rectangle const& rectangle, Transform const& rectangle_transform, Rotation const& rectangle_rotation)
+{
+  auto const top_right =
+    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x + rectangle.width_height.x * 0.5f,
+                                               rectangle_transform.value.y + rectangle.width_height.y * 0.5f },
+                                 rectangle_transform.value,
+                                 rectangle_rotation.degrees);
+  auto const top_left =
+    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x - rectangle.width_height.x * 0.5f,
+                                               rectangle_transform.value.y + rectangle.width_height.y * 0.5f },
+                                 rectangle_transform.value,
+                                 rectangle_rotation.degrees);
+  auto const bottom_right =
+    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x + rectangle.width_height.x * 0.5f,
+                                               rectangle_transform.value.y - rectangle.width_height.y * 0.5f },
+                                 rectangle_transform.value,
+                                 rectangle_rotation.degrees);
+  auto const bottom_left =
+    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x - rectangle.width_height.x * 0.5f,
+                                               rectangle_transform.value.y - rectangle.width_height.y * 0.5f },
+                                 rectangle_transform.value,
+                                 rectangle_rotation.degrees);
+
+  return { top_right, top_left, bottom_right, bottom_left };
+}
+
+/// The four segments that make up a rectangle's outline
+inline std::vector<Segment>
+  segments(Rectangle const& rectangle, Transform const& rectangle_transform, Rotation const& rectangle_rotation)
+{
+  auto const top_right =
+    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x + rectangle.width_height.x * 0.5f,
+                                               rectangle_transform.value.y + rectangle.width_height.y * 0.5f },
+                                 rectangle_transform.value,
+                                 rectangle_rotation.degrees);
+  auto const top_left =
+    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x - rectangle.width_height.x * 0.5f,
+                                               rectangle_transform.value.y + rectangle.width_height.y * 0.5f },
+                                 rectangle_transform.value,
+                                 rectangle_rotation.degrees);
+  auto const bottom_right =
+    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x + rectangle.width_height.x * 0.5f,
+                                               rectangle_transform.value.y - rectangle.width_height.y * 0.5f },
+                                 rectangle_transform.value,
+                                 rectangle_rotation.degrees);
+  auto const bottom_left =
+    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x - rectangle.width_height.x * 0.5f,
+                                               rectangle_transform.value.y - rectangle.width_height.y * 0.5f },
+                                 rectangle_transform.value,
+                                 rectangle_rotation.degrees);
+
+  return { Segment{ .start = top_right, .end = bottom_right },
+           Segment{ .start = bottom_right, .end = bottom_left },
+           Segment{ .start = bottom_left, .end = top_left },
+           Segment{ .start = top_left, .end = top_right } };
+}
+}// namespace rectangle_utils
