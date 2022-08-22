5837bfd6c1936c4b7cf6c8eb9d4f1e5074ccb507
Harrison Marshall <harry130899@hotmail.com>
2022-08-22 13:42:44 +1000

    Engine: Fixed rotation unit tests

--- engine/tests/rotation.cpp
+++ engine/tests/rotation.cpp
@@ -7,8 +7,19 @@
 
 TEST(Rotation, RotationsAboutPoint)
 {
-  auto const result = rotation::rotate_about_point(sf::Vector2f{ 2.f, 2.f }, sf::Vector2f{ 0.f, 0.f }, 90);
+  auto const error_threshold = 0.05f;
 
-  EXPECT_FLOAT_EQ(result.x, 2.f);
-  EXPECT_FLOAT_EQ(result.y, -2.f);
+  {
+    auto const result = rotation::rotate_about_point(sf::Vector2f{ 2.f, 2.f }, sf::Vector2f{ 0.f, 0.f }, 90);
+
+    EXPECT_NEAR(result.x, 2.f, error_threshold);
+    EXPECT_NEAR(result.y, -2.f, error_threshold);
+  }
+
+  {
+    auto const result = rotation::rotate_about_point(sf::Vector2f{ 0.f, 1.f }, sf::Vector2f{ 0.f, 0.f }, 10);
+
+    EXPECT_NEAR(result.x, 0.17365f, error_threshold);
+    EXPECT_NEAR(result.y, 0.98481f, error_threshold);
+  }
 }
