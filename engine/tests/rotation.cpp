3861e41eaf27bc7a2c407075a948edffc3d7491f
Harrison Marshall <harry130899@hotmail.com>
2022-08-23 13:52:36 +1000

    Engine: Fixed rotation test

--- engine/tests/rotation.cpp
+++ engine/tests/rotation.cpp
@@ -12,14 +12,14 @@ TEST(Rotation, RotationsAboutPoint)
   {
     auto const result = rotation::rotate_about_point(sf::Vector2f{ 2.f, 2.f }, sf::Vector2f{ 0.f, 0.f }, 90);
 
-    EXPECT_NEAR(result.x, 2.f, error_threshold);
-    EXPECT_NEAR(result.y, -2.f, error_threshold);
+    EXPECT_NEAR(result.x, -2.f, error_threshold);
+    EXPECT_NEAR(result.y, 2.f, error_threshold);
   }
 
   {
     auto const result = rotation::rotate_about_point(sf::Vector2f{ 0.f, 1.f }, sf::Vector2f{ 0.f, 0.f }, 10);
 
-    EXPECT_NEAR(result.x, 0.17365f, error_threshold);
+    EXPECT_NEAR(result.x, -0.17365f, error_threshold);
     EXPECT_NEAR(result.y, 0.98481f, error_threshold);
   }
 }
