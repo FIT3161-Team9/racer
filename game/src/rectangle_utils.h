0c4aa4199bf3108185bf5d6018a0f73253612932
Kurii <marcus.kuri@y7mail.com>
2022-08-24 14:20:03 +1000

    Game: Cleaned up rectangle_utils::corners

--- game/src/rectangle_utils.h
+++ game/src/rectangle_utils.h
@@ -12,30 +12,52 @@
 namespace rectangle_utils
 {
 
+inline sf::Vector2f
+  top_left_corner(Rectangle const& rectangle, Transform const& rectangle_transform, Rotation const& rectangle_rotation)
+{
+  return rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x - rectangle.width_height.x * 0.5f,
+                                                    rectangle_transform.value.y - rectangle.width_height.y * 0.5f },
+                                      rectangle_transform.value,
+                                      rectangle_rotation.degrees);
+};
+inline sf::Vector2f
+  top_right_corner(Rectangle const& rectangle, Transform const& rectangle_transform, Rotation const& rectangle_rotation)
+{
+  return rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x + rectangle.width_height.x * 0.5f,
+                                                    rectangle_transform.value.y - rectangle.width_height.y * 0.5f },
+                                      rectangle_transform.value,
+                                      rectangle_rotation.degrees);
+}
+
+inline sf::Vector2f bottom_right_corner(Rectangle const& rectangle,
+                                        Transform const& rectangle_transform,
+                                        Rotation const& rectangle_rotation)
+{
+  return rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x + rectangle.width_height.x * 0.5f,
+                                                    rectangle_transform.value.y + rectangle.width_height.y * 0.5f },
+                                      rectangle_transform.value,
+                                      rectangle_rotation.degrees);
+}
+
+inline sf::Vector2f bottom_left_corner(Rectangle const& rectangle,
+                                       Transform const& rectangle_transform,
+                                       Rotation const& rectangle_rotation)
+{
+  return rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x - rectangle.width_height.x * 0.5f,
+                                                    rectangle_transform.value.y + rectangle.width_height.y * 0.5f },
+                                      rectangle_transform.value,
+                                      rectangle_rotation.degrees);
+}
+
 /// The four corners of a rectangle
 inline std::vector<sf::Vector2f>
   corners(Rectangle const& rectangle, Transform const& rectangle_transform, Rotation const& rectangle_rotation)
 {
-  auto const top_right =
-    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x + rectangle.width_height.x * 0.5f,
-                                               rectangle_transform.value.y + rectangle.width_height.y * 0.5f },
-                                 rectangle_transform.value,
-                                 rectangle_rotation.degrees);
-  auto const top_left =
-    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x - rectangle.width_height.x * 0.5f,
-                                               rectangle_transform.value.y + rectangle.width_height.y * 0.5f },
-                                 rectangle_transform.value,
-                                 rectangle_rotation.degrees);
-  auto const bottom_right =
-    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x + rectangle.width_height.x * 0.5f,
-                                               rectangle_transform.value.y - rectangle.width_height.y * 0.5f },
-                                 rectangle_transform.value,
-                                 rectangle_rotation.degrees);
-  auto const bottom_left =
-    rotation::rotate_about_point(sf::Vector2f{ rectangle_transform.value.x - rectangle.width_height.x * 0.5f,
-                                               rectangle_transform.value.y - rectangle.width_height.y * 0.5f },
-                                 rectangle_transform.value,
-                                 rectangle_rotation.degrees);
+  auto const top_right = top_right_corner(rectangle, rectangle_transform, rectangle_rotation);
+  auto const top_left = top_left_corner(rectangle, rectangle_transform, rectangle_rotation);
+
+  auto const bottom_right = bottom_right_corner(rectangle, rectangle_transform, rectangle_rotation);
+  auto const bottom_left = bottom_left_corner(rectangle, rectangle_transform, rectangle_rotation);
 
   return { top_right, top_left, bottom_right, bottom_left };
 }
