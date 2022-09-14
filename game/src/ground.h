058d2635c0eaa39e897291aa2bc9d6384cb04675
Harrison Marshall <harry130899@hotmail.com>
2022-09-14 13:53:19 +1000

    Game: Removed a bunch of debug prints

--- game/src/ground.h
+++ game/src/ground.h
@@ -29,18 +29,9 @@ inline void circle_ground_collision_plugin(AppCommands& app_commands)
           collisions::rectangle_circle(ground_rectangle, ground_transform, ground_rotation, circle, circle_transform);
 
         if (collision_result) {
-          std::cout << "Collision test for circle at: ";
-          print_vector(circle_transform.value);
-          std::cout << "\nGround at: ";
-          print_vector(ground_transform.value);
-          std::cout << "(rotation: " << ground_rotation.degrees << ")\n";
-
-
           auto const depth_of_collision = circle.radius
                                           - vector_utils::magnitude(vector_utils::minus(
                                             collision_result->point_of_collision, circle_transform.value));
-
-          std::cout << "Depth of collision: " << depth_of_collision << "\n";
 
           auto const depth_of_collision_in_direction_of_velocity = rotation::rotate_about_point(
             vector_utils::scale_vector(
@@ -49,39 +40,18 @@ inline void circle_ground_collision_plugin(AppCommands& app_commands)
             sf::Vector2f{ 0.f, 0.f },
             180.f);
 
-          std::cout << "Depth of collision in direction of velocity: ";
-          print_vector(depth_of_collision_in_direction_of_velocity);
-          std::cout << "\n";
-
           circle_transform.value.x += depth_of_collision_in_direction_of_velocity.x;
           circle_transform.value.y += depth_of_collision_in_direction_of_velocity.y;
-
-          std::cout << "Updated transform\n";
-          if (collisions::rectangle_circle(
-                ground_rectangle, ground_transform, ground_rotation, circle, circle_transform)) {
-            std::cout << "Still colliding after updating transform...\n";
-          }
 
           const auto dot = vector_utils::dot_product(circle_velocity.value, collision_result->normal);
           const auto new_velocity = vector_utils::scale_vector(
             vector_utils::minus(circle_velocity.value, vector_utils::scale_vector(collision_result->normal, 2 * dot)),
             0.75f);
 
-          std::cout << "New velocity: ";
-          print_vector(new_velocity);
-          std::cout << "\n\n";
-
           circle_velocity.value = new_velocity;
 
-          std::cout << "Intermediate transform: ";
-          print_vector(circle_transform.value);
-          std::cout << "\n";
           circle_transform.value.x += new_velocity.x * elapsed_time.elapsed_milliseconds * 0.001f;
           circle_transform.value.y += new_velocity.y * elapsed_time.elapsed_milliseconds * 0.001f;
-
-          std::cout << "Final transform: ";
-          print_vector(circle_transform.value);
-          std::cout << "\n";
         }
       }
     }
