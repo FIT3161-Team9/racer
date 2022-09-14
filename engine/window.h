9bcd99e7a18ecb86ceb1645306d32c9438110a99
Harrison Marshall <harry130899@hotmail.com>
2022-09-14 13:52:54 +1000

    Engine: Slightly refactored window resize code

--- engine/window.h
+++ engine/window.h
@@ -30,28 +30,39 @@ inline sf::Vector2f from_screen_space(sf::Vector2f const& vec)
   return { vec.x - 0.5f * COORDINATE_SPACE_WIDTH, vec.y - 0.5f * COORDINATE_SPACE_HEIGHT };
 }
 
+/// Given a rectangle and a given aspect ratio, return the amount you would have to scale the rectangle by
+/// in each dimension to have it equal to the given aspect ratio
+inline sf::Vector2f scale_required_to_satisfy_aspect_ratio(sf::Vector2f const& rectangle, float const aspect_ratio)
+{
+  auto const width = rectangle.x;
+  auto const height = rectangle.y;
+  auto const scaled_height = width / aspect_ratio;
+
+  auto const target_width = scaled_height > height ? height * aspect_ratio : width;
+  auto const target_height = scaled_height > height ? height : scaled_height;
+
+  auto const target_width_scale = target_width / width;
+  auto const target_height_scale = target_height / height;
+
+  return { target_width_scale, target_height_scale };
+}
+
 /// Scale the render view to fit the current window size. This basically ensures
 /// that the aspect ratio of the view maintains consistent as the user resizes
 /// the window
 inline void scale_view_to_window_size(sf::RenderWindow& window)
 {
   auto const [width, height] = window.getSize();
-
-  auto const scaled_height = static_cast<float>(width) / window::ASPECT_RATIO;
-
-  // If the scaled height is greater than the height of the window, we want to scale
-  // the width
-  auto const target_width = scaled_height > height ? height * window::ASPECT_RATIO : static_cast<float>(width);
-  auto const target_height = scaled_height > height ? height : scaled_height;
-
-  auto const target_width_scale = target_width / width;
-  auto const target_height_scale = target_height / height;
+  auto [target_width_scale, target_height_scale] = scale_required_to_satisfy_aspect_ratio(
+    { static_cast<float>(width), static_cast<float>(height) }, window::ASPECT_RATIO);
 
   sf::View view(sf::FloatRect(0, 0, window::COORDINATE_SPACE_WIDTH, window::COORDINATE_SPACE_HEIGHT));
 
   // Scale the width and height based on the aspect ratio
-  view.setViewport(
-    { (1 - target_width_scale) * 0.5f, (1 - target_height_scale) * 0.5f, target_width_scale, target_height_scale });
+  auto const viewport = sf::Rect<float>{
+    (1 - target_width_scale) * 0.5f, (1 - target_height_scale) * 0.5f, target_width_scale, target_height_scale
+  };
+  view.setViewport(viewport);
 
   window.setView(view);
 }
