55ef82799ad50685aff0a97d6da052f4d3bbf6e5
Harrison Marshall <harry130899@hotmail.com>
2022-10-01 13:00:05 +1000

    Game: Added level argument

--- game/src/arguments.h
+++ game/src/arguments.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <cstring>
+#include <iostream>
 
 struct Arguments
 {
   bool debug_mode;
+  char* level;
 };
 
 namespace arguments
@@ -14,9 +16,21 @@ inline Arguments parse(int argc, char** argv)
 {
   auto arguments = Arguments{ .debug_mode = false };
 
-  if (argc != 2) { return arguments; }
+  if (argc < 2) { return arguments; }
 
-  arguments.debug_mode = strcmp(argv[1], "-d") || strcmp(argv[1], "--debug");
+  int i = 1;
+  while (i < argc) {
+    if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--debug") == 0) { arguments.debug_mode = true; }
+
+    if (strcmp(argv[i], "--level") == 0 && i < argc - 1) {
+      arguments.level = argv[i + 1];
+      i += 2;
+      continue;
+    }
+
+    i++;
+  }
+
 
   return arguments;
 }
