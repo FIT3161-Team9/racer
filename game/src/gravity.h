53a151062e825b90040b4347737f31c93c82edb0
Harrison Marshall <harry130899@hotmail.com>
2022-10-01 11:23:18 +1000

    Game: Respect the pause state in debug mode

--- game/src/gravity.h
+++ game/src/gravity.h
@@ -3,6 +3,7 @@
 #include "engine/app_commands.h"
 #include "engine/query.h"
 #include "engine/timing.h"
+#include "game/src/debug/pausable.h"
 #include "game/src/velocity.h"
 
 enum class AffectedByGravity {};
@@ -15,14 +16,18 @@ float constexpr GRAVITY = 9.8f;
 inline void plugin(AppCommands& app_commands)
 {
 
-  app_commands.add_system(
-    ResourceQuery<timing::ElapsedTime>{}, Query<AffectedByGravity, Velocity>{}, [](auto& resources, auto& view) {
-      auto&& [_, elapsed_time] = resources;
+  app_commands.add_system(ResourceQuery<timing::ElapsedTime>{},
+                          Query<AffectedByGravity, Velocity>{},
+                          [&app_commands](auto& resources, auto& view) {
+                            auto* pause_state = app_commands.get_resource<debug::pausable::PauseState>();
+                            if (pause_state != nullptr && pause_state->paused) { return; }
 
-      for (auto&& [_entity, _affected_by_gravity, velocity] : view.each()) {
-        velocity.value.y += elapsed_time.elapsed_milliseconds * GRAVITY;
-      }
-    });
+                            auto&& [_, elapsed_time] = resources;
+
+                            for (auto&& [_entity, _affected_by_gravity, velocity] : view.each()) {
+                              velocity.value.y += elapsed_time.elapsed_milliseconds * GRAVITY;
+                            }
+                          });
 }
 
 }// namespace gravity
