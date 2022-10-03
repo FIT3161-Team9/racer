8131c45b01cdbf4afc69aa31a1e8203ffbcd9930
Harrison Marshall <harry130899@hotmail.com>
2022-10-03 18:07:39 +1100

    Game: Save finish line location to level json
    We also now serialize the vehicle spawn position

--- game/src/currently_loaded_map.h
+++ game/src/currently_loaded_map.h
@@ -2,10 +2,14 @@
 
 #include <string>
 
+#include "SFML/System/Vector2.hpp"
+
 #include "engine/app_commands.h"
 
 struct CurrentlyLoadedMap
 {
   std::string name;
   int version;
+  float finish_line_location;
+  sf::Vector2f vehicle_spawn_location;
 };
