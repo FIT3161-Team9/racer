bb7e4c01be85f4a7cae686ecc696079c67317103
cdin0019 <cdin0019@student.monash.edu>
2022-10-04 12:58:18 +1100

    add vehicle select functionality
    Co-authored-by: Yanchen Pan <ypan0045@student.monash.edu>

--- game/src/game_state.h
+++ game/src/game_state.h
@@ -6,5 +6,13 @@ struct GameState
   /// The current screen to be rendered
   enum class CurrentScreen { SplashScreen, MainMenu, VehicleSelect, ResultScreen, DisplayCourse, InLevel };
 
+  enum class CurrentMap {Map1, Map2};
+
+  enum class CurrentVehicle {Vehicle1, Vehicle2, Vehicle3};
+
   CurrentScreen current_screen;
+
+  CurrentMap current_map;
+
+  CurrentVehicle current_vehicle;
 };
