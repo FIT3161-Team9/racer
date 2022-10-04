d61d9f87f037269cd334a5743965114654ce6cc1
Harrison Marshall <harry130899@hotmail.com>
2022-10-04 20:21:07 +1100

    Game: Fixed compile errors

--- game/src/game_state.h
+++ game/src/game_state.h
@@ -4,19 +4,27 @@
 struct GameState
 {
   /// The current screen to be rendered
-  enum class CurrentScreen { SplashScreen, MainMenu, VehicleSelect1, VehicleSelect2, ResultScreen, DisplayCourse, InLevel };
+  enum class CurrentScreen {
+    SplashScreen,
+    MainMenu,
+    VehicleSelect1,
+    VehicleSelect2,
+    ResultScreen,
+    DisplayCourse,
+    InLevel
+  };
 
-  enum class CurrentMap {Map1, Map2};
+  enum class CurrentMap { Map1, Map2 };
 
-  enum class CurrentVehicle_p1 {Vehicle1, Vehicle2, Vehicle3};
+  enum class CurrentVehicle_p1 { Vehicle1, Vehicle2, Vehicle3 };
 
-  enum class CurrentVehicle_p2 {Vehicle1, Vehicle2, Vehicle3};
+  enum class CurrentVehicle_p2 { Vehicle1, Vehicle2, Vehicle3 };
 
-  CurrentScreen current_screen;
+  CurrentScreen current_screen{};
 
-  CurrentMap current_map;
+  CurrentMap current_map{};
 
-  CurrentVehicle_p1 current_vehicle_p1;
+  CurrentVehicle_p1 current_vehicle_p1{};
 
-  CurrentVehicle_p2 current_vehicle_p2;
+  CurrentVehicle_p2 current_vehicle_p2{};
 };
